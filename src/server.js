e8d02769397da82e638846fc713561553d0a0b20
Adrian Majcher <majcherrr87@gmail.com>
2026-04-29 15:24:57 +0200

    cron.js added

--- src/server.js
+++ src/server.js
@@ -4,16 +4,22 @@ import { initDB } from "./config/db.js";
 import ratelimiter from "./middleware/rateLimiter.js";
 
 import transacrionsRoute from "./routes/transactionsRoute.js";
+import job from "./config/cron.js";
 
 dotenv.config();
 
 export const app = express();
+if (process.env.NODE_ENV === "production") job.start();
 
 //middleware
 app.use(ratelimiter);
 app.use(express.json());
 
 const PORT = process.env.PORT || 5001;
+
+app.get("/api/test", (req, res) => {
+  res.status(200).json({ status: "ok" });
+});
 
 app.use("/api/transactions", transacrionsRoute);
 
